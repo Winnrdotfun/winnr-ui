62a3595bfe249503281cc8c9d5e350a526585913
nvnx7 <thenvnsahu@gmail.com>
2025-05-03 14:37:50 +0530

    chore: add config

---
+++ src/config/env.ts
@@ -0,0 +1,5 @@
+export const env = "localnet"; // localnet, devnet, mainnet
+
+export const RPC_LOCALNET = "http://localhost:8899";
+export const RPC_DEVNET =
+  "https://devnet.helius-rpc.com/?api-key=074f9832-beac-403b-a698-4c3ed9045065";
