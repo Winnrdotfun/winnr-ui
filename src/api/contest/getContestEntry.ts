dd01a7eacab33c4a1fe8aede8325f09588bd3254
nvnx7 <thenvnsahu@gmail.com>
2025-05-04 03:04:24 +0530

    feat: add contest entry api

---
+++ src/api/contest/getContestEntry.ts
@@ -0,0 +1,31 @@
+import { Program, web3 } from "@coral-xyz/anchor";
+import type { Protocol as IWinner } from "../idl/winnr";
+import { programId } from "../utils";
+
+export const getTokenDraftContestsEntry = async (
+  pg: Program<IWinner>,
+  params: { contestAddress: string; signerAddress: string }
+) => {
+  const { contestAddress } = params;
+  const [contestEntryPda] = web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("token_draft_contest_entry"),
+      new web3.PublicKey(contestAddress).toBuffer(),
+      new web3.PublicKey(params.signerAddress).toBuffer(),
+    ],
+    programId
+  );
+
+  const entry = await pg.account.tokenDraftContestEntry.fetch(contestEntryPda);
+
+  if (!entry) {
+    return null;
+  }
+
+  return {
+    user: entry.user.toBase58(),
+    id: entry.id,
+    contestAddress: entry.contestKey.toBase58(),
+    hasClaimed: entry.hasClaimed,
+  };
+};
