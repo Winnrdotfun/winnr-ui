b0c9195d60b7cceaad4a397a92b538af216b1059
nvnx7 <thenvnsahu@gmail.com>
2025-05-07 17:33:14 +0530

    fix: temporarily use public rpc

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,6 @@
 export const env = "devnet"; // localnet, devnet, mainnet
 
 export const rpcLocalnet = process.env.NEXT_PUBLIC_RPC_LOCALNET as string;
-export const rpcDevnet = process.env.NEXT_PUBLIC_RPC_DEVNET as string;
+// export const rpcDevnet = process.env.NEXT_PUBLIC_RPC_DEVNET as string;
+export const rpcDevnet = "https://api.devnet.solana.com";
 export const rpcMainnet = process.env.NEXT_PUBLIC_RPC_MAINNET as string;
