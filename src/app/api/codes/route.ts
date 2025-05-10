779098944bd315aadb88003697a854617e57207a
nvnx7 <thenvnsahu@gmail.com>
2025-05-10 23:45:43 +0530

    feat: add GET invite code call

--- src/app/api/codes/route.ts
+++ src/app/api/codes/route.ts
@@ -3,6 +3,35 @@ export const dynamic = "force-static";
 
 const connectionString = process.env.DATABASE_URL as string;
 
+export async function GET(request: Request) {
+  const sql = postgres(connectionString);
+  const params = new URL(request.url).searchParams;
+
+  // Extract walletAddress from the query parameters
+  const walletAddress = params.get("walletAddress");
+
+  if (!walletAddress) {
+    return new Response("Invalid parameters: walletAddress is required", {
+      status: 400,
+    });
+  }
+
+  // Query the database using walletAddress
+  const codeData = (
+    await sql`SELECT invite_code, wallet_address FROM winnr_codes WHERE wallet_address = ${walletAddress}`
+  )?.[0];
+
+  if (!codeData) {
+    return new Response("Wallet address not found", { status: 404 });
+  }
+
+  // Return the matching record
+  return Response.json({
+    inviteCode: codeData.invite_code,
+    walletAddress: codeData.wallet_address,
+  });
+}
+
 export async function POST(request: Request) {
   const sql = postgres(connectionString);
   const params = await request.json();
